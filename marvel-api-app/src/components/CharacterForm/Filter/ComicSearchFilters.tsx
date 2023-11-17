ee7ff787b5d6c8ea741654cf9e8e0995ce6cd2d3
Tyler Metz <tmbs9963@gmail.com>
2023-11-17 11:31:26 -0500

    include variants param

--- marvel-api-app/src/components/CharacterForm/Filter/ComicSearchFilters.tsx
+++ marvel-api-app/src/components/CharacterForm/Filter/ComicSearchFilters.tsx
@@ -6,13 +6,22 @@ type ComicSearchFiltersProps = {
 };
 
 const ComicSearchFilters = ({ onFilterChange }: ComicSearchFiltersProps) => {
-  const [selectedOrderBy, setSelectedOrderBy] = useState('');
+  const [selectedOrderBy, setSelectedOrderBy] = useState<string>('');
+  const [selectedIncludeVariants, setSelectedIncludeVariants] = useState<boolean>(false);
 
   const handleOrderByChange = (orderBy: string) => {
     setSelectedOrderBy(orderBy);
     onFilterChange({
       orderBy,
-      includeVariants: false
+      includeVariants: selectedIncludeVariants,
+    });
+  };
+
+  const handleIncludeVariantsChange = (includeVariants: boolean) => {
+    setSelectedIncludeVariants(includeVariants);
+    onFilterChange({
+      orderBy: selectedOrderBy,
+      includeVariants,
     });
   };
 
@@ -23,27 +32,27 @@ const ComicSearchFilters = ({ onFilterChange }: ComicSearchFiltersProps) => {
         <select
           value={selectedOrderBy}
           onChange={(e) => handleOrderByChange(e.target.value)}
-          >
-            <option value={''}>None</option>
-            <option value={'onSaleDate'}>On Sale Date</option>
-            <option value={'focDate'}>FOC(Final Order Cutoff) Date</option>
-            <option value={'title'}>Title</option>
-            <option value={'issueNumber'}>Issue Number</option>
-            <option value={'-onSaleDate'}>- On Sale Date</option>
-            <option value={'-focDate'}>- FOC(Final Order Cutoff) Date</option>
-            <option value={'-title'}>- Title</option>
-            <option value={'-issueNumber'}>- Issue Number</option>
-          </select>
+        >
+          <option value={''}>None</option>
+          <option value={'onSaleDate'}>On Sale Date</option>
+          <option value={'focDate'}>FOC(Final Order Cutoff) Date</option>
+          <option value={'title'}>Title</option>
+          <option value={'issueNumber'}>Issue Number</option>
+          <option value={'-onSaleDate'}>- On Sale Date</option>
+          <option value={'-focDate'}>- FOC(Final Order Cutoff) Date</option>
+          <option value={'-title'}>- Title</option>
+          <option value={'-issueNumber'}>- Issue Number</option>
+        </select>
       </label>
       <label>
-       Include Variants:
+        Include Variants:
         <select
-          value={selectedOrderBy}
-          onChange={(e) => handleOrderByChange(e.target.value)}
-          >
-            <option value={'true'}>True</option>
-            <option value={'false'}>False</option>
-          </select>
+          value={selectedIncludeVariants.toString()}
+          onChange={(e) => handleIncludeVariantsChange(e.target.value === 'true')}
+        >
+          <option value={'true'}>True</option>
+          <option value={'false'}>False</option>
+        </select>
       </label>
     </div>
   );
